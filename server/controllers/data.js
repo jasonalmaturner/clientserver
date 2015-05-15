6d8771127ac08f19dbe3cbe7a40f8762a0f3200d
Jacob Turner <j.israel.turner@gmail.com>
2015-05-15 15:46:41 -0600

    Adds get total responses.

--- server/controllers/data.js
+++ server/controllers/data.js
@@ -1,6 +1,14 @@
-import { getClientData } from './../services/data-service';
+import { getResults } from './../services/data-service';
 
 function total(req, res){
+  if(
+    !req.query.tenant_id
+  ) return res.status(400).send('Insufficient Parameters');
+  else {
+    getResults({ tenant_id: req.query.tenant_id })
+      .then(results => res.json(results))
+      .catch(err => res.status(500).json(err));
+  };
 
 };
 
@@ -10,7 +18,7 @@ function client(req, res){
     !req.query.tenant_id
   ) return res.status(400).send('Insufficient Parameters');
   else {
-    getClientData(req.query)
+    getResults({ client_id: req.query.client_id, tenant_id: req.query.tenant_id })
       .then(results => res.json(results))
       .catch(err => res.status(500).json(err));
   };
--- server/services/data-service.js
+++ server/services/data-service.js
@@ -3,27 +3,29 @@ import { r } from './../configuration/database';
 
 var surveys = r.table('surveys');
 
-function getClientData(obj){
+function getResults(obj){
   var dfd = q.defer();
   var year = r.now().date().year(); 
   surveys
     .getAll(obj.tenant_id, { index: 'tenant_id' })
     .filter( r.row('date').year().eq(year))
     .run()
-    .then(res => _parseClients({ surveys: res, client_id: obj.client_id }))
+    .then(res => _parseContacts({ surveys: res, client_id: obj.client_id }))
     .then(res => _calcNps(res))
     .then(res => dfd.resolve(res))
     .catch(err => dfd.reject(err));
   return dfd.promise;
 };
 
-function _parseClients(obj){
+function _parseContacts(obj){
   var dfd = q.defer();
   var results = [];
 
   obj.surveys.forEach((item, index) => {
     item.clients.forEach((item, index) => {
-      if(item.client_id == obj.client_id){
+      if(obj.client_id && item.client_id == obj.client_id){
+        results = results.concat(item.contacts);
+      } else if (!obj.client_id){
         results = results.concat(item.contacts);
       }
     })
@@ -53,4 +55,4 @@ function _calcNps(responses){
   return dfd.promise;
 }
 
-export { getClientData };
+export { getResults };
