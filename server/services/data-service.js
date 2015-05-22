a55b32e862c2a910f32e051cbd0e6856deb563d7
Jacob Turner <j.israel.turner@gmail.com>
2015-05-22 17:05:41 -0600

    Working on finding quarters.

--- server/services/data-service.js
+++ server/services/data-service.js
@@ -6,6 +6,7 @@ var surveys = r.table('surveys');
 function getResults(obj){
   var dfd = q.defer();
   var year = r.now().date().year(); 
+  var quarter = _getQuarter(obj.current);
   surveys
     .getAll(obj.tenant_id, { index: 'tenant_id' })
     .filter( r.row('date').year().eq(year))
@@ -35,6 +36,67 @@ function _parseContacts(obj){
 
   return dfd.promise;
 };
+
+function _getQuarter(current){
+  var year = new Date().getFullYear();
+  var month = new Date().getMonth() + 1;
+  var results = {
+    current: {
+      from: null,
+      to: null
+    },
+    previous: {
+      from: null,
+      to: null
+    }
+  }
+ 
+
+  switch (month){
+    case 1 :
+    case 2 :
+    case 3 :
+      results.current.from = makeDate(false, 1,1,year);
+      results.current.to = makeDate(true, 3,31,year);
+      results.previous.from = makeDate(false, 10,1,year-1);
+      results.previous.to = makeDate(true, 12,31,year-1);
+      break;
+    case 4 :
+    case 5 :
+    case 6 :
+      results.current.from = makeDate(false, 4,1,year);
+      results.current.to = makeDate(true, 6,30,year);
+      results.previous.from = makeDate(false, 1,1,year);
+      results.previous.to = makeDate(true, 3,31,year);
+      break;
+    case 7 :
+    case 8 :
+    case 9 :
+      results.current.from = makeDate(false, 7,1,year);
+      results.current.to = makeDate(true, 9,30,year);
+      results.previous.from = makeDate(false, 4,1,year);
+      results.previous.to = makeDate(true, 6,30,year);
+      break;
+    case 10 :
+    case 11 :
+    case 12 :
+      results.current.from = makeDate(false, 10,1,year);
+      results.current.to = makeDate(true, 12,31,year);
+      results.previous.from = makeDate(false, 7,1,year);
+      results.previous.to = makeDate(true, 9,30,year);
+      break;
+  }
+
+  function makeDate(last, month, day, year){
+    month = month - 1;
+    if(!last) return new Date(year, month, day);
+    else return new Date(year, month, day, 23, 59, 59);
+    // Needs to be 23:59:59 if the last day of the year;
+  }
+  
+
+  console.log(results);
+}
 
 function _calcNps(responses){
   var dfd = q.defer();
